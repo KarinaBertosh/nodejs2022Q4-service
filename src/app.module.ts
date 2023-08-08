6d4905a6ffdbaa832d34dff7c58193f347773e06
KarinaBertosh <cherrycake246@gmail.com>
2023-08-08 16:53:27 +0300

    feat: add db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { DbModule } from './database/db.module';
 import { UserModule } from './modules/users/user.module';
 import { TrackModule } from './modules/tracks/track.module';
@@ -9,13 +10,25 @@ import { FavoriteModule } from './modules/favorite/favorite.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
     UserModule,
     TrackModule,
     ArtistModule,
     AlbumModule,
     FavoriteModule,
     DbModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRESS_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRESS_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [],
+      synchronize: true,
+    }),
   ],
 })
 export class AppModule {}
