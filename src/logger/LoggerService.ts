8f72ee0e7456cd2f0398846d24ad6505f0428cb3
KarinaBertosh <cherrycake246@gmail.com>
2023-08-22 11:07:00 +0300

    feat: add custom message

--- src/logger/LoggerService.ts
+++ src/logger/LoggerService.ts
@@ -1,23 +1,27 @@
-import { ConsoleLogger } from '@nestjs/common';
+import { ConsoleLogger, LogLevel } from '@nestjs/common';
 
 export class MyLogger extends ConsoleLogger {
-  error(message: any, stack?: string, context?: string) {
-    console.log(message);
+  error(message: any) {
+    console.log(this.customMessage(message, 'error'));
   }
 
-  log(message: any, stack?: string, context?: string) {
-    console.log(message);
+  log(message: any) {
+    console.log(this.customMessage(message, 'log'));
   }
 
-  warn(message: any, stack?: string, context?: string) {
-    console.log(message);
+  warn(message: any) {
+    console.log(this.customMessage(message, 'warn'));
   }
 
-  debug(message: any, stack?: string, context?: string) {
-    console.log(message);
+  debug(message: any) {
+    console.log(this.customMessage(message, 'debug'));
   }
 
-  verbose(message: any, stack?: string, context?: string) {
-    console.log(message);
+  verbose(message: any) {
+    console.log(this.customMessage(message, 'verbose'));
+  }
+
+  private customMessage(message: string, logLevel: LogLevel) {
+    return `${logLevel}: ` + message;
   }
 }
