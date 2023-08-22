11e13422946142726aaafcad07af1166f6df3d74
KarinaBertosh <cherrycake246@gmail.com>
2023-08-22 17:30:30 +0300

    fix: delete import HttpExceptionFilter

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { MyLogger } from './logger/LoggerService';
-import { HttpExceptionFilter } from './errors/http-exception.filter';
 
 const PORT = process.env['PORT'];
 
@@ -12,8 +11,7 @@ async function bootstrap() {
   });
   const logger = app.get(MyLogger);
   app.useLogger(logger);
-  app.useGlobalFilters(new HttpExceptionFilter());
-
+  
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(PORT);
 }
